2516a7ff38bca785f003cd501e9767a3b85b23cb
oliverj053 <oliver_js597@outlook.com>
2019-05-27 12:26:05 -0500

    cnn

--- componentes/basededatos/conexion.php
+++ componentes/basededatos/conexion.php
@@ -1,8 +1,5 @@
 <?php
-$db="pizzeria";
-$host="localhost";
-$pass="";
-$user="root";
+
     $conexion = new mysqli($host,$user, $pass,$db);
     if($conexion->connect_errno){
         echo "Error al conectarse con la base de datos: ".$conexion->connect_error;
