56c1ad7a04b25ecffe5a85fd3b340d94aa8e2072
HOODY\HP <HoodyB>
2019-05-28 08:01:37 -0500

    Librerías para el diseño menú

--- componentes/home/home/menu/pizzas.php
+++ componentes/home/home/menu/pizzas.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../../../css/pizzas.css">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" 
     integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
