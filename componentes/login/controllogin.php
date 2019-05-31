da1f87e61ebc9eed0c196dbf5fa7c1eb062ae630
Besiddes <Besiddes1@gmail.com>
2019-05-31 10:25:17 -0500

    control_login_useralreadyregistred

--- componentes/login/controllogin.php
+++ componentes/login/controllogin.php
@@ -3,6 +3,7 @@ require '../basededatos/conexion.php';
 
 
     $email=$_POST['user'];
+    
     $pass=$_POST['pass'];
 
     $sql=$conexion->query("SELECT tipo_user from clientes where (email_cliente='$email' and pass_cliente='$pass') ");
