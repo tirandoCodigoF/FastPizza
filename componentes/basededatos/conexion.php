39d7e2f2416aa2b305a795c79b162eac3b55ff5e
Besiddes <Besiddes1@gmail.com>
2019-05-31 13:46:14 -0500

    conexionerror

--- componentes/basededatos/conexion.php
+++ componentes/basededatos/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$db="bzxlwzvexbaiqu1sypi9";
-$host="bzxlwzvexbaiqu1sypi9-mysql.services.clever-cloud.com";
+$db="b4cclr8mz6enuymcjuik";
+$host="b4cclr8mz6enuymcjuik-mysql.services.clever-cloud.com";
 $pass="Od6JyCvE855NKM4JfVwv";
-$user="u1htrxt785hqk9gr";
+$user="Ga1IvlnpTE1vVk9xtA0F";
     $conexion = new mysqli($host,$user, $pass,$db);
     if($conexion->connect_errno){
         echo "Error al conectarse con la base de datos: ".$conexion->connect_error;
