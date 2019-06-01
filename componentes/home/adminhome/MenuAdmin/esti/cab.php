8d8376839693e9502a6b98e62bce108da3a9a45c
tirandoCodigoF <44149781+tirandoCodigoF@users.noreply.github.com>
2019-06-01 00:33:26 -0500

    se eliminoaddminhome

--- componentes/home/adminhome/MenuAdmin/esti/cab.php
+++
@@ -1,6 +0,0 @@
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-	<link rel="stylesheet" href="css/dataTables.bootstrap.min.css">
-	<link rel="stylesheet" href="css/estilos.css">
-	<!-- Buttons DataTables -->
-	<link rel="stylesheet" href="css/buttons.bootstrap.min.css">
-	<link rel="stylesheet" href="css/font-awesome.min.css">
--- componentes/home/adminhome/controlador/Conne.php
+++
@@ -1,4 +0,0 @@
-<?php 
-    include_once 'Conexionn.php';
-    $con = Conexionn::getConn();
-?>
