339f9b12f9399128af51eb8ee939c4c41f752be2
HOODY\HP <HoodyB>
2019-06-01 20:29:57 -0500

    Cliente_index principal

--- componentes/home/home/menu/index.php
+++ componentes/home/home/menu/index.php
@@ -24,10 +24,27 @@ require ('../../../navbar/index.php');
  <main>
   <section class="acerca-de">
    <div class="contenedor">
-    <h2 class="sobre-nosotros">Sobre Nosotros</h2>
+    <h2 class="sobre-nosotros">Sobre Nosotros</h2><hr>
+    
     <h3 class="slogan">Star-Soft</h3>
-    <p class="parrafo">Hola ipsum dolor sit amet consectetur, adipisicing elit. Unde exercitationem id optio repudiandae accusantium? Reprehenderit repellendus alias deserunt porro, pariatur beatae incidunt. Voluptatibus sunt ut quo quod molestiae ab architecto unde corporis adipisci, temporibus, commodi deleniti saepe placeat eaque nisi impedit quia officia, cumque excepturi veritatis animi? Delectus eos voluptatum earum nihil, similique ut dolor. Culpa voluptas cum reiciendis, eligendi vero magnam voluptatibus obcaecati illo facere recusandae vel. Sequi, architecto!</p>
-    <p class="parrafo">Hola ipsum dolor sit amet consectetur adipisicing elit. Ipsum, quas maxime. Aut tempore consequatur adipisci dolorum ut autem tenetur a, aliquam eligendi dolor expedita vel pariatur optio harum? Quibusdam dolorem cum dignissimos animi atque! Deleniti modi dignissimos est dolore ad provident? Totam nesciunt velit ipsum quaerat placeat similique, soluta distinctio. Fugit deleniti eius officia iste, excepturi iusto consequatur corrupti numquam minus ut soluta aliquam quam molestias, ipsam mollitia aliquid corporis.</p>
+    <p class="parrafo">La Pizzería *****, es considerada la mejor de la ciudad, gracias a que su preparación que es muy meticulosa 
+    y su sabor increíble; en el proceso de Elaboración, está el secreto nuestro, tenemos creaciones propias de pizzas que solo encontrará 
+    en nuestros locales, que son un  producto artesanal, logrado con ingredientes y productos de máxima calidad. Somos más que una pizzería, 
+    ya somos una  tradición. </p><hr>
+    
+    <h3 class="slogan">Misión</h3>
+    <p class="parrafo">Brindar un servicio de calidad, proporcionando a nuestros clientes una variedad de ofertas que
+     permitan a nuestro público disfrutar de productos propios e innovadores, en un mercado establecido, con una delicada 
+     elaboración con ingredientes de primera calidad, a un precio razonable y competitivo, de esa manera superar las expectativas 
+     de nuestro público consumidor.</p><hr>
+     
+     <h3 class="slogan">Misión</h3>
+    <p class="parrafo">Brindar un servicio de calidad, proporcionando a nuestros clientes una variedad de ofertas que
+     permitan a nuestro público disfrutar de productos propios e innovadores, en un mercado establecido, con una delicada 
+     elaboración con ingredientes de primera calidad, a un precio razonable y competitivo, de esa manera superar las expectativas 
+     de nuestro público consumidor.</p><hr>
+
+</p>
     <a href="#" class="boton">Home</a>
    </div>
   </section>
